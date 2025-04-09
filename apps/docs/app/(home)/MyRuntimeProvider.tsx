065339e9a5551a6af42fc8615fbb6c17e0bd08c1
Simon Farshid <simon.farshid@outlook.com>
2025-04-08 17:11:36 -0700

    feat: dummy chat adapter for website (#1799)

--- apps/docs/app/(home)/MyRuntimeProvider.tsx
+++ apps/docs/app/(home)/MyRuntimeProvider.tsx
@@ -5,18 +5,49 @@ import {
   SimpleTextAttachmentAdapter,
   AssistantRuntimeProvider,
 } from "@assistant-ui/react";
-import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
+import {
+  type ChatModelAdapter,
+  type ChatModelRunOptions,
+  useLocalRuntime,
+} from "@assistant-ui/react";
 
-export const MyRuntimeProvider = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  const runtime = useChatRuntime({
-    api: "/api/chat",
-    body: {
-      apiKey: process.env["NEXT_PUBLIC_BACKEND_API_KEY"],
-    },
+async function* tokenByToken(str: string) {
+  for (const token of str.split(" ")) {
+    await new Promise((resolve) => setTimeout(resolve, Math.random() * 10 + 5));
+    yield token + " ";
+  }
+}
+
+const LOREM_IPSUM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+class DummyChatAdapter implements ChatModelAdapter {
+  async *run({ messages, abortSignal }: ChatModelRunOptions) {
+    const text =
+      messages.length === 1
+        ? "This is a mocked chat endpoint for testing purposes. \n\n" +
+          LOREM_IPSUM
+        : LOREM_IPSUM;
+    let output = "";
+
+    for await (const token of tokenByToken(text)) {
+      abortSignal.throwIfAborted();
+
+      output += token;
+
+      yield {
+        content: [
+          {
+            type: "text" as const,
+            text: output,
+          },
+        ],
+      };
+    }
+  }
+}
+
+export function MyRuntimeProvider({ children }: { children: React.ReactNode }) {
+  const runtime = useLocalRuntime(new DummyChatAdapter(), {
     adapters: {
       attachments: new CompositeAttachmentAdapter([
         new SimpleImageAttachmentAdapter(),
@@ -35,4 +66,4 @@ export const MyRuntimeProvider = ({
       {children}
     </AssistantRuntimeProvider>
   );
-};
+}
